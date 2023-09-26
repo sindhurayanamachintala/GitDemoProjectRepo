73b5e585ae4ab7d800ab0ccbf1a43223de014f39
sindhura <sindhurayanamachintala123@gmail.com>
2023-09-26 17:18:38 +0530

    commited testclass2 file and changing in brachA

--- src/Package1/Testclass2.java
+++ src/Package1/Testclass2.java
@@ -4,7 +4,7 @@ public class Testclass2 {
 
 	public static void main(String[] args) {
 
-		String hungry= "yes";
+		String hungry= "y";
 		if(hungry=="yes") {
 			System.out.println("eat something");
 		}
